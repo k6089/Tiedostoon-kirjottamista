f5cb493e706623532db9a3de242d57e2cf3bb678
Akseli Koskela <playester@gmail.com>
2016-02-03 13:28:14 +0200

    Demo08

---
+++ Demo08/CalculatorApplication/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    interface ICalculator
+    {
+        int Add(int number1, int number2);
+        int Multiple(int number1, int number2);
+
+    }
+}
