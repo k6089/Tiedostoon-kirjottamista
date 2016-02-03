cac89ab9b245f45535807c52246f473a1a41c6fb
Akseli Koskela <playester@gmail.com>
2016-02-03 15:26:54 +0200

    T2 alkua

---
+++ T2-ostokset/T2-ostokset/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_ostokset
+{
+    class Product
+    {
+        public string Ostos { get; set; }
+        public double Hinta { get; set; }
+
+        public override string ToString()
+        {
+            return "Ostos : " + Ostos + " " + Hinta + "e";
+        }
+    }
+}
