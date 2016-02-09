1de5f66a5eeadbd79cc09c9e1e9c1d9ac5764395
Akseli Koskela <playester@gmail.com>
2016-02-09 08:16:47 +0200

    T5Laskin

---
+++ T5Laskin/T5/ArrayCalcs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    public class ArrayCalcs
+    {
+        public static void Lasku(double[] x)
+        {
+            double[] array = x;
+
+            // double Sum = array.Sum();
+            // double Average = array.Average();
+            // double Min = array.Min();
+            // double Max = array.Max();
+
+            Console.WriteLine("Sum = {0:f2}", array.Sum());
+            Console.WriteLine("Ave = {0:f2}", array.Average());
+            Console.WriteLine("Min = {0:f2}", array.Min());
+            Console.WriteLine("Max = {0:f2}", array.Max());
+        }
+    }
+
+}
---
+++ T5Laskin/T5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = { 1.0, 2.0, 3.3, 5.5, 6.3, -4.5, 12.0 };
+
+            ArrayCalcs.Lasku(array);
+        }
+    }
+}
