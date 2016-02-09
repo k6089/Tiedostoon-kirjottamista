9c5ebe92504fdc76ed3482debc2462d0ff6256f8
Akseli Koskela <playester@gmail.com>
2016-02-09 08:21:19 +0200

    T6Kauppa

---
+++ T6Kuitti/T6/Pohja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    class Pohja
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return Name + "\t" + Price + "e " + Quantity + " pieces " + Price * Quantity + " total";
+        }
+    }
+}
---
+++ T6Kuitti/T6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pohja> kuitti = new List<Pohja>();
+
+            kuitti.Add(new Pohja { Name = "Milk", Price = 1.75, Quantity = 1 });
+            kuitti.Add(new Pohja { Name = "Beer", Price = 5.25, Quantity = 1 });
+            kuitti.Add(new Pohja { Name = "Butter", Price = 2.52, Quantity = 1 });
+
+            string Customer;
+
+            Console.Write("Who was shopping ?  ");
+            Customer = Console.ReadLine();
+
+
+
+            Console.Clear();
+
+            Console.WriteLine("Customer " + Customer + "'s invoice:\n=================================");
+
+            foreach (Pohja summa in kuitti)
+            {
+                Console.WriteLine(summa.ToString());
+            }
+            Console.WriteLine("=================================");
+
+        }
+    }
+}
