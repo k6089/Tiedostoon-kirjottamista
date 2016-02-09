e38e48ed4f34e796a992c0cd8b72e16d95201c7e
Akseli Koskela <playester@gmail.com>
2016-02-09 08:10:01 +0200

    T2 loppuun

--- T2-ostokset/T2-ostokset/Program.cs
+++ T2-ostokset/T2-ostokset/Program.cs
@@ -10,6 +10,18 @@ namespace T2_ostokset
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product { Ostos = "Milk", Hinta = 1.4 });
+            products.Add(new Product { Ostos = "Beer", Hinta = 2.2 });
+            products.Add(new Product { Ostos = "Butter", Hinta = 3.2 });
+            products.Add(new Product { Ostos = "Cheese", Hinta = 4.2 });
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.ToString());
+            }
+
         }
     }
 }
